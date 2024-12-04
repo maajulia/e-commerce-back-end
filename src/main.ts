86aa81baacd7949a3a036d5c9f32c1b6259bb734
maajulia <118356114+maajulia@users.noreply.github.com>
2024-12-04 11:50:45 -0400

    alteração dos links

--- src/main.ts
+++ src/main.ts
@@ -44,22 +44,6 @@ app.post("/roupas", async (req, res) => {
     }
 })
 
-app.get("/lista", async (req, res) => {
-    try {
-        const connection = await mysql.createConnection({
-            host: process.env.dbhost ? process.env.dbhost : "localhost",
-            user: process.env.dbuser ? process.env.dbuser : "root",
-            password: process.env.dbpassword ? process.env.dbpassword : "",
-            database: process.env.dbname ? process.env.dbname : "banco1022a",
-            port: process.env.dbport ? parseInt(process.env.dbport) : 3306
-        })
-        const [result, fields] = await connection.query("SELECT * from lista")
-        await connection.end()
-        res.send(result)
-    } catch (e) {
-        res.status(500).send("Server ERROR")
-    }
-})
 
 app.listen(8000, () => {
     console.log("Iniciei o servidor")
